181d46534dd13cb6438b3924ce243be89b408de3
soumensardar <soumen.sardar@smithsdetection.com>
2023-12-13 11:37:09 +0530

    train script for sixraysd3

--- experiments/run_sixraysd3.sh
+++ experiments/run_sixraysd3.sh
@@ -2,4 +2,4 @@
 
 python train.py --dataset sixray_sd3                \
 --isize 128 --niter 100 --model skipganomaly        \
---batchsize 32 --dataroot /data/sixray_sd3_anomaly
+--batchsize 32 --dataroot /data/sixray_sd3_anomaly --display
