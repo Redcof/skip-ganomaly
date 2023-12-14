8141c5ccb66ae1a0d126b78137ccf92f682cc9e2
soumensardar <soumen.sardar@smithsdetection.com>
2023-12-14 17:42:15 +0530

    script update to add experiment name

--- experiments/run_sixraysd3.sh
+++ experiments/run_sixraysd3.sh
@@ -2,4 +2,5 @@
 
 python train.py --dataset sixray_sd3                \
 --isize 128 --niter 100 --model ganomaly        \
+--name sixray_sd3_experiment\
 --batchsize 32 --dataroot /data/sixray_sd3_anomaly --display
