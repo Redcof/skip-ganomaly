0ecdad930b1b37c3b7feafa76468ae47c9b39e1a
soumensardar <soumen.sardar@smithsdetection.com>
2023-12-28 15:00:36 +0530

    change experiment name

--- experiments/run_sixraysd3_nc1_px64_traycrop_gun.sh
+++ experiments/run_sixraysd3_nc1_px64_traycrop_gun.sh
@@ -2,5 +2,5 @@
 
 python train.py --dataset sixray_sd3                  \
 --isize 64 --niter 75 --manualseed 47 --model ganomaly              \
---name sixraysd3_nc1_px64_traycrop_gun     --nc 1          \
+--name exp5_sixraysd3_ep75_nc1_px64_traycrop_gun     --nc 1          \
 --batchsize 32 --dataroot /data/sixray_sd3_anomaly --display
